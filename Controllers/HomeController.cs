5a8b304cfa2757047f811576f362f210f30e99db
sagarrd <sagar.dudhaiya@outlook.com>
2022-01-02 14:14:33 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KEService.Services;
+using KEService.Models;
+using System.Net;
+
+namespace KEService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private APIResponce apiresponce;
+
+        [HttpGet("getpersonlist")]
+        public IActionResult GetPersonData()
+        {
+            apiresponce = new APIResponce();
+
+            PersonService objPersonService = new PersonService();
+
+            var response = objPersonService.GetPersonList();
+
+            if (response == null)
+            {
+                apiresponce.StatusCode = (int)HttpStatusCode.BadRequest;
+                apiresponce.Data = response;
+            }
+            else
+            {
+                apiresponce.StatusCode = (int)HttpStatusCode.OK;
+                apiresponce.Data = response;
+            }
+
+            return new JsonResult(apiresponce);
+        }
+
+        [HttpPost("getpersondatabyid")]
+        public IActionResult GetPersonById(PersonModel model)
+        {
+            PersonService objPersonService = new PersonService();
+
+            var response = objPersonService.GetPersonDataById(model.Oid);
+
+            if (response == null)
+                return BadRequest(new { message = "No Record found" });
+
+            int status = (int)HttpStatusCode.OK;
+            return Ok(response);
+        }
+    }
+}
---
+++ Models/Data/SQLConnection.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace KEService.Models.Data
+{
+    public class SQLConnection
+    {
+
+        static string _staticConnectionString;
+
+        public static SqlConnection StaticSqlConnection
+        {
+            get
+            {
+
+                SqlConnection staticConnection = new SqlConnection();
+                staticConnection.ConnectionString = StaticConnectionString;
+                return staticConnection;
+            }
+        }
+
+        public static string StaticConnectionString
+        {
+            set { _staticConnectionString = value; }
+            get
+            {
+                if (!string.IsNullOrEmpty(_staticConnectionString))
+                    return _staticConnectionString;
+
+                string con = ConfigurationManager.AppSetting["ConnectionStrings:KeConnection"];
+                //string con = ConfigurationManager.AppSetting["KeConnection"];
+
+                return con;
+            }
+
+
+        }
+    }
+}
